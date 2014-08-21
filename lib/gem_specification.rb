146024d042a38d9292a768747f2e97b33332ecf8
Danijel Ilisin <di@diltec.de>
2014-08-21 16:41:34 +0200

    Added Licensed under ... license regex - works for Apache 2.0 license

--- lib/gem_specification.rb
+++ lib/gem_specification.rb
@@ -5,7 +5,7 @@ class Gem::Specification
     ary = (__licenses || []).keep_if { |l| l.length > 0 }
     ary.length == 0 ? guess_licenses : ary
   end
-  
+
   def guess_licenses
     licenses = []
     Dir.foreach(full_gem_path) do |filename|
@@ -27,7 +27,7 @@ class Gem::Specification
   end
 
   private
-  
+
   def guess_licenses_from_file_contents(path)
     licenses = []
     file_handle = File.new(path, "r")
@@ -35,13 +35,14 @@ class Gem::Specification
       while (line = file_handle.gets) && (licenses.size == 0)
         line = line.strip
         # positive matches
-        [ 
+        [
           /released under the (?<l>[\s\w]*) license/i,
           /same license as (?<l>[\s\w]*)/i,
           /^(?<l>[\s\w]*) License, see/i,
           /^(?<l>[\w]*) license$/i,
           /\(the (?<l>[\s\w]*) license\)/i,
           /^license: (?<l>[\s\w]*)/i,
+          /^licensed under the (?<l>[\s\w]*) license/i,
           /^released under the (?<l>[\s\w]*) license/i,
         ].each do |r|
           res = Regexp.new(r).match(line)
@@ -56,6 +57,6 @@ class Gem::Specification
     file_handle.close
     licenses
   end
-  
+
 end
 
